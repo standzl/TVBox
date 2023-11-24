df1c7f955b57c87b4bd58e0b9aff8ac8381136b7
sunt <standzl1949@gmail.com>
2023-11-24 14:26:23 +0800

    add config;

--- app/src/main/java/com/github/tvbox/osc/bbox/player/TrackInfoBean.java
+++
@@ -1,8 +0,0 @@
-package com.github.tvbox.osc.bbox.player;
-
-public class TrackInfoBean {
-    public String name;
-    public String language;
-    public int index;
-    public boolean selected;
-}
